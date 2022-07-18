17011f07f49a20e5662d12b1594139ad402203e3
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-18 14:06:06 -0700

    update styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import Coins from "./components/Coins";
 import NavBar from "./components/NavBar";
+import './index.css';
 
 function App() {
 
