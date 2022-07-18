7cd3079ff04296bd5bff9ae840993b07b2ee7922
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-18 13:45:55 -0700

    update coins.js and url for api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import Coins from "./components/Coins";
 
 function App() {
 
   const [coins, setCoins] = useState([]);
+  
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+    axios.get(url)
       .then((res) => {
         setCoins(res.data)
         console.log(res.data[0])
@@ -15,7 +18,7 @@ function App() {
 
   return (
     <>
-    dick and balls
+    <Coins coins={coins} />
     </>
   );
 }
